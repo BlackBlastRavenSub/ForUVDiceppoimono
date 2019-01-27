e36735b530ecd11310dca4030078448bc52a4dbe
KOGUMA <smallbear0608@gmail.com>
2019-01-28 00:20:24 +0900

    //データベースで問題発生

--- app/src/main/java/com/blastraven/b/foruvdice/Roll.kt
+++ app/src/main/java/com/blastraven/b/foruvdice/Roll.kt
@@ -6,11 +6,11 @@ class Roll(num: Int) {
     var result = 0//ダイスロールの結果は!?
     var numface = num//ダイスの面数
     var commonmultiple = 0//最小公倍数
-    var Percentparam = Array<Int>(numface + 1) { 0 }//百分率の配列
-    var Outcome = 0//結果
-    var directrand = (0..numface).random()//乱数
-    var Percentagerand = (1..100).random()//乱数
-    var commonmultiplerand = 0//乱数
+    var percentParam = Array<Int>(numface + 1) { 0 }//百分率の配列
+    var outcome = 0//結果
+    var directRand = (0..numface).random()//乱数
+    var percentageRand = (1..100).random()//乱数
+    var commonmultipleRand = 0//乱数
     fun ClosedRange<Int>.random() =
             Random().nextInt((endInclusive + 1) - start) + start
 
@@ -20,48 +20,62 @@ class Roll(num: Int) {
         //各種初期化
         numface = num//ダイスの面数
         commonmultiple = 0//最小公倍数
-        Percentparam = Array<Int>(numface + 1) { 0 }//百分率の配列
-        Outcome = 0//結果
-        directrand = (0..numface).random()//乱数
-        Percentagerand = (1..100).random()//乱数
-        commonmultiplerand = 0//乱数
+        percentParam = Array<Int>(numface + 1) { 0 }//百分率の配列
+        outcome = 0//結果
+        directRand = (0..numface).random()//乱数
+        percentageRand = (1..100).random()//乱数
+        commonmultipleRand = 0//乱数
 
         //いざ転がす!
-        //テスト用、まずは最も単純なDirectRollを使ってみようと思ったが、自分でDirectRollの使い方を忘れてしまっているためやっぱりPercentageRollを使用する
+        //テスト用、まずは前回と同じ計算方式のPercentageRollを使用する
         PercentageRoll(numface)
         return result
     }
 
     //確率を直接表記
-    fun DirectRoll(tossnum: Int) {
-        var numface = tossnum
-        directrand = (1..numface).random()
+    fun DirectRoll(tossNum: Int) {
+        var numface = tossNum
+        directRand = (1..numface).random()
         for (i in 1..numface) {
-            when (directrand) {
-                i -> Outcome = i
+            when (directRand) {
+                i -> outcome = i
             }
         }
     }
 
     //確率を百分率で判定
-    fun PercentageRoll(tossnum: Int) {
-        this.numface = tossnum
-        Percentagerand = (1..100).random()
+    fun PercentageRoll(tossNum: Int) {
+        val test = Array(7) { i -> i + 1 }
+        this.numface = tossNum
+        percentageRand = (1..100).random()
         for (i in 1..numface) {
-            when (Percentagerand) {
-                in Percentparam[i - 1]..Percentparam[i] -> Outcome = i
+            when (percentageRand) {
+                in percentParam[i - 1]..percentParam[i] -> outcome = i
+                //in test[i - 1]..test[i] -> outcome = i+1
             }
         }
-        result = Outcome
+        result = outcome
     }
+    /*
+     fun PercentageRoll(tossnum: Int) {
+        this.numface = tossnum
+        percentageRand = (1..100).random()
+        for (i in 1..numface) {
+            when (percentageRand) {
+                in percentParam[i - 1]..percentParam[i] -> outcome = i
+            }
+        }
+        result = outcome
+    }
+     */
 
     //確率を最小公倍数で判定
     fun commonmultipleRoll(tossnum: Int) {
         this.numface = tossnum
-        commonmultiplerand = (0..commonmultiple).random()
+        commonmultipleRand = (0..commonmultiple).random()
         for (i in 1..numface) {
-            when (commonmultiplerand) {
-                in 2..10 -> Outcome = i
+            when (commonmultipleRand) {
+                in 2..10 -> outcome = i
             }
         }
     }
